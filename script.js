6dfa1c19a1621d323b7caf8df274a8d76307f187
vendyggod <charli98@mail.ru>
2023-09-29 22:05:50 +0300

    keep learning

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ createUsernames(accounts);
 
 const calcDisplayBalance = movements => {
   const balance = movements.reduce((summ, mov) => summ + mov, 0);
-  labelBalance.textContent = `${balance}€`
+  labelBalance.textContent = `${balance}€`;
 };
 
 calcDisplayBalance(account1.movements);
@@ -137,8 +137,11 @@ const currencies = new Map([
 // .reduce(accumulator, current element, index, array) 0 в качестве второго аргумента - это стартовая позиция счётчика
 
 // Maximum value of the array
-const maxValue = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0]);
-console.log(maxValue)
+const maxValue = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
+console.log(maxValue);
 
 // const depositsFor = [];
 // // for (const mov of movements) if (mov > 0) depositsFor.push(); для сравнения как бы for loop
@@ -171,3 +174,62 @@ console.log(maxValue)
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// const calcAverageHumanAge = ages => {
+//   const humanAges = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+//   return humanAges;
+// };
+
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+// const summ = arr => {
+//   const arrCopy = arr.slice().sort();
+//   const arrUnique = [];
+
+//   for (let i = 0; i < arrCopy.length; i++) {
+//     if (arrCopy[i] !== arrCopy[i - 1] && arrCopy[i] !== arrCopy[i + 1])
+//       arrUnique.push(arrCopy[i]);
+//   }
+//   return arrUnique.reduce((acc, num) => acc + num, 0);
+// };
+
+// console.log(summ([6, 3, 4, 3, 3, 4, 4, 6, 3, 4, 6])); // = > 10
+// console.log(summ([5, 6, 10, 3, 10, 10, 6, 7, 0, 9, 1, 1, 6, 3, 1]));
+
+// const summ2 = arr => {
+//   return arr
+//     .filter(num => arr.indexOf(num) === arr.lastIndexOf(num))
+//     .reduce((acc, num) => acc + num, 0);
+// };
+
+// console.log(summ2([6, 3, 4, 3, 3, 4, 4, 6, 3, 4, 6])); // = > 10
+// console.log(summ2([5, 6, 10, 3, 10, 10, 6, 7, 0, 9, 1, 1, 6, 3, 1]));
+
+// function secondLargest(array) {
+//   const arrCopy =
+//     typeof array === 'object'
+//       ? [...new Set(array)].filter(el => typeof el === 'number' || 0).sort()
+//       : undefined;
+//   console.log(arrCopy);
+//   return arrCopy && arrCopy.length > 1
+//     ? Number(arrCopy.slice(-2)[0])
+//     : undefined;
+// }
+
+// console.log(secondLargest([-32, 11, 43, 55, 0, 11]));
+// console.log(secondLargest(['-1', 2, null, false]));
+// console.log(secondLargest([2, true, 0]));
+// console.log(secondLargest('32 11 44 56'));
+// console.log(secondLargest([0, 1, '2', 3]));
+
+// Test.assertEquals(secondLargest([-32,11,43,55,0,11]), 43);
+// Test.assertEquals(secondLargest(null), undefined);
+// Test.assertEquals(secondLargest([5,5,5,5,5,5]), undefined);
+// Test.assertEquals(secondLargest([0, 1, '2', 3]), 2);
+// Test.assertEquals(secondLargest(['-1', 2, null, false]), -1);
+
+console.log(typeof [2, 3, '2']);
